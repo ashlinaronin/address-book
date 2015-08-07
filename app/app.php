8b297e867a3a3150bdc782c95f7e4a48059a8947
Ashlin Aronin <ashlinaronin@gmail.com>
2015-08-07 10:43:27 -0700

    Add comments to app.php, remove test code

--- app/app.php
+++ app/app.php
@@ -17,17 +17,15 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
-    // Root path displays any existing Contacts and supplies entry form
+    // Display any existing Contacts and contact entry form
     $app->get('/', function() use ($app) {
         return $app['twig']->render('address_book.html.twig', array(
             'list_of_contacts' => Contact::getAll()
         ));
     });
 
+    // Create new Contact object and present confirmation page to user
     $app->post('/create_contact', function() use ($app) {
-
-        // add input validation in constructor
-
         $new_contact = new Contact($_POST['name'], $_POST['phone'], $_POST['address']);
         $new_contact->save();
 
@@ -37,21 +35,9 @@
         ));
     });
 
-    $app->get('/tests', function() use ($app) {
-        $output = '';
-
-        foreach (Contact::getAll() as $current_contact) {
-            $output .= "Contact name: " . $current_contact->getName();
-            $output .= ", phone: " . $current_contact->getPhone();
-            $output .= ", address: " . $current_contact->getAddress();
-            $output .= "<hr>";
-        }
-
-        return $output;
-    });
-
-    /* Delete contacts route doesn't need to pass any data to Twig because
-       nothing particular to the user's address book is displayed. */
+    /* Delete all contacts and present confirmation page to user.
+       This route doesn't need to pass any data to Twig because
+       nothing particular to the user is displayed, just HTML. */
     $app->get('/delete_contacts', function() use ($app) {
         Contact::deleteAll();
         return $app['twig']->render('delete_contacts.html.twig');
